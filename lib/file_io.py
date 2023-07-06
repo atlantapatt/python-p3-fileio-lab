5ae6784092d951e9a8508a5adf86e5addb0de3c6
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-07-06 16:26:24 -0700

    second function

--- lib/file_io.py
+++ lib/file_io.py
@@ -3,7 +3,8 @@ def write_file(file_name, file_content):
         f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
     pass
